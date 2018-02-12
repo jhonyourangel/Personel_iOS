6f4b1b619c9db5491978104dfecfca4b04a6400f
ion utale <ion.utale@icloud.com>
2018-02-12 14:08:12 +0100

    add project class and view

--- Personel/Projects/AddProjectVC.swift
+++ Personel/Projects/AddProjectVC.swift
@@ -14,13 +14,53 @@ class AddProjectVC: ViewController {
     @IBOutlet weak var incomeTF: UITextField!
     @IBOutlet weak var saveBtn: TransitionButton!
     
+    var project: Project!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nameTF.text = project.name
+        incomeTF.text = "\(project.income!)"
     }
     
     @IBAction func saveProject() {
-        
+        if project._id == nil {
+            addProject()
+            return
+        }
+        editProject()
     }
+    
+    func addProject() {
+        saveBtn.startAnimation()
+        self.startLoader()
+        Network.addProject(name: nameTF.text!, income: Int(incomeTF.text!)!) { (proj, statusCode, error) in
+            self.saveBtn.stopAnimation()
+            self.stopLoader()
+            if error != nil {
+                self.presentBanner(title: "error", message: (error?.localizedDescription)!)
+                return
+            }
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    func editProject() {
+        saveBtn.startAnimation()
+        self.startLoader()
+        Network.editProject(id: project._id!, name: nameTF.text!, income: Int(incomeTF.text!)!) { (proj, statusCode, error) in
+            self.saveBtn.stopAnimation()
+                self.stopLoader()
+                if error != nil {
+                    self.presentBanner(title: "error", message: (error?.localizedDescription)!)
+                    return
+                }
+                // maybe pop back at this point
+            self.navigationController?.popViewController(animated: true)
+
+            }
+        }
+    
 }
 
 extension AddProjectVC: UITextFieldDelegate {
