0207708e24197b639401eececd48246353474ab9
ion utale <ion.utale@icloud.com>
2018-02-12 14:08:25 +0100

    projects list

--- Personel/Projects/ProjectsVC.swift
+++ Personel/Projects/ProjectsVC.swift
@@ -11,14 +11,39 @@ import TransitionButton
 
 class ProjectsVC: ViewController {
     @IBOutlet weak var tableView: UITableView!
-
+    var projects: [Project]! = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
-    @IBAction func addProject(_ sender: Any) {
-        self.performSegue(withIdentifier: "addProject", sender: self)
+    override func viewDidAppear(_ animated: Bool) {
+        self.startLoader()
+        Network.getProjects() { (proj, sc, error) in
+            if error != nil {
+                self.presentBanner(title: "Error", message: "unable to get projects.\(error?.localizedDescription ?? "")")
+                return
+            }
+            self.projects = proj
+            UserManager.projects = proj
+            self.tableView.reloadData()
+            self.stopLoader()
+        }
+    }
+    
+    @IBAction func createProject() {
+        let project = Project()
+        project.income = 0
+        project.name = ""
+        self.performSegue(withIdentifier: "addProject", sender: project)
+    }
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "addProject" {
+            let destination = segue.destination as! AddProjectVC
+            destination.project = sender as! Project
+        }
     }
 }
 
@@ -33,11 +58,18 @@ extension ProjectsVC: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return projects.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = projects[indexPath.row].name
+        cell?.detailTextLabel?.text = "\(projects[indexPath.row].income!)"
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "addProject", sender: projects[indexPath.row])
     }
 }
