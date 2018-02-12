8bf1871ba3c1b0f3ed550dd668f262cb7f235e8a
ion utale <ion.utale@icloud.com>
2018-02-12 14:06:52 +0100

    add, edit project api call functions added

--- Personel/Network/Network+Project.swift
+++ Personel/Network/Network+Project.swift
@@ -7,10 +7,11 @@
 //
 
 import Foundation
+import Alamofire
 
 extension Network {
     
-    static func getTransactions(completion: @escaping([Project]?, Int?, Error?) -> ()) {
+    static func getProjects(completion: @escaping([Project]?, Int?, Error?) -> ()) {
         let URL = URLs.projects
         
         sessionManager.request(URL, method: .get, parameters: [:], encoding: URLEncoding.default )
@@ -39,4 +40,73 @@ extension Network {
                 }
             })
     }
+    
+    static func addProject(name: String, income: Int,completion: @escaping(Project?, Int?, Error?) -> ()) {
+        let URL = URLs.addProject
+        let parameters: Parameters = [
+            "name": name,
+            "income": income
+        ]
+        sessionManager.request(URL, method: .post, parameters: parameters, encoding: URLEncoding.default )
+            //.validate()
+            .responseJSON(completionHandler: { response in
+                let statusCode = response.response?.statusCode
+                switch response.result {
+                case .success(let _):
+                    guard let data = response.data else {
+                        completion(nil, statusCode, ErrorModel.defError())
+                        return
+                    }
+                    do {
+                        let item = try JSONDecoder().decode(Project.self, from: data)
+                        completion(item, statusCode, nil)
+                    } catch let jsonError {
+                        print(data.base64EncodedString().base64Decoded(), jsonError)
+                        completion( nil, statusCode, jsonError as NSError)
+                    }
+                case .failure(let error):
+                    if let data = response.data {
+                        print(error.localizedDescription, statusCode)
+                        completion(nil, statusCode, getNSError( data: data) ?? error as NSError)
+                    } else {
+                        completion(nil, statusCode, error as NSError)
+                    }
+                }
+            })
+    }
+    
+    static func editProject(id: String,name: String, income: Int,completion: @escaping(Project?, Int?, Error?) -> ()) {
+        let URL = URLs.editProject
+        let parameters: Parameters = [
+            "id": id,
+            "name": name,
+            "income": income
+        ]
+        sessionManager.request(URL, method: .put, parameters: parameters, encoding: URLEncoding.default )
+            //.validate()
+            .responseJSON(completionHandler: { response in
+                let statusCode = response.response?.statusCode
+                switch response.result {
+                case .success(let _):
+                    guard let data = response.data else {
+                        completion(nil, statusCode, ErrorModel.defError())
+                        return
+                    }
+                    do {
+                        let item = try JSONDecoder().decode(Project.self, from: data)
+                        completion(item, statusCode, nil)
+                    } catch let jsonError {
+                        print(data.base64EncodedString().base64Decoded(), jsonError)
+                        completion( nil, statusCode, jsonError as NSError)
+                    }
+                case .failure(let error):
+                    if let data = response.data {
+                        completion(nil, statusCode, getNSError( data: data) ?? error as NSError)
+                    } else {
+                        completion(nil, statusCode, error as NSError)
+                    }
+                }
+            })
+    }
+    
 }
