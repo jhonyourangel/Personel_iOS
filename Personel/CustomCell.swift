94dc6edeb4d6bac5cfdac656e65b332094dd6a04
ion utale <ion.utale@icloud.com>
2018-02-27 14:32:01 +0100

    created custom cell

---
+++ Personel/CustomCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCell.swift
+//  Personel
+//
+//  Created by Ion Utale on 26/02/2018.
+//  Copyright © 2018 Ion Utale. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTAppleCell {
+    @IBOutlet weak var dateLabel: UILabel!
+    
+}
